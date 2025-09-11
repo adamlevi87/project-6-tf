b601f143dd9429dcde8b8621e761f134943d231d
Adam Levi <adamlevi87@gmail.com>
2025-09-11 13:00:09 +0300

    eksremoved output duplication

--- modules/eks/cluster/outputs.tf
+++ modules/eks/cluster/outputs.tf
@@ -32,11 +32,6 @@ output "cluster_security_group_id" {
   value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = aws_eks_cluster.main.name
-}
-
 output "cluster_endpoint" {
   description = "EKS cluster endpoint"
   value       = aws_eks_cluster.main.endpoint
