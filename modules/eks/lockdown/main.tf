8912deaf169e6877174c16e63c774aeda5d6e63f
Adam Levi <adamlevi87@gmail.com>
2025-09-12 20:37:25 +0300

    testing lockdown

--- modules/eks/lockdown/main.tf
+++ modules/eks/lockdown/main.tf
@@ -12,22 +12,22 @@ terraform {
 # Trigger GitHub workflow to lockdown EKS access
 resource "null_resource" "trigger_eks_lockdown" {
   provisioner "local-exec" {
-    command = <<-EOT
-      echo "Triggering EKS lockdown workflow..."
-      curl -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${var.github_token}" \
-        https://api.github.com/repos/${var.github_org}/${var.github_repo}/actions/workflows/lockdown-eks.yml/dispatches \
-        -d '{
-          "ref": "main",
-          "inputs": {
-            "cluster_security_group_id": "${var.cluster_security_group_id}",
-            "trigger_source": "terraform"
-          }
-        }'
-      echo "EKS lockdown workflow trigger sent"
-    EOT
-  }
+  command = <<-EOT
+    echo "Triggering EKS lockdown workflow..."
+    curl -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${var.github_token}" \
+      https://api.github.com/repos/${var.github_org}/${var.github_repo}/actions/workflows/lockdown-eks.yml/dispatches \
+      -d '{
+        "ref": "main",
+        "inputs": {
+          "cluster_security_group_id": "${var.cluster_security_group_id}",
+          "trigger_source": "terraform"
+        }
+      }'
+    echo "EKS lockdown workflow trigger sent"
+  EOT
+}
   
   triggers = {
     cluster_sg_id = var.cluster_security_group_id
