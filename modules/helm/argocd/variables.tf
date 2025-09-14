0993a720d6c09c7c0b3c010b104c78a1c63a41af
Adam Levi <adamlevi87@gmail.com>
2025-09-14 15:59:25 +0300

    edited var tf argocd

--- modules/helm/argocd/variables.tf
+++ modules/helm/argocd/variables.tf
@@ -15,11 +15,6 @@ variable "alb_security_groups" {
   type        = string
 }
 
-# variable "frontend_security_group_id" {
-#   description = "ID of the security group for the frontend"
-#   type        = string
-# }
-
 variable "namespace" {
   type        = string
   description = "The Kubernetes namespace to install the Helm release into"
@@ -29,28 +24,6 @@ variable "github_org" {
   description = "GitHub organization"
   type        = string
 }
-
-# variable "github_application_repo" {
-#   description = "GitHub repository name"
-#   type        = string
-# }
-
-# variable "github_gitops_repo" {
-#   description = "GitHub repository name"
-#   type        = string
-# }
-
-# variable "app_of_apps_path" {
-#   description = "Path within the GitOps repository where ArgoCD should look for Application manifests."
-#   type        = string
-#   default     = "apps"
-# }
-
-# variable "app_of_apps_target_revision" {
-#   description = "Branch or Git reference in the GitOps repository that ArgoCD should track."
-#   type        = string
-#   default     = "main"
-# }
 
 variable "release_name" {
   type        = string
@@ -129,16 +102,6 @@ variable "argocd_app_of_apps_yaml" {
   description = "Rendered App-of-Apps YAML from argocd-templates module"
   type        = string
 }
-
-# variable "vpc_id" {
-#   description = "VPC ID where RDS will be deployed"
-#   type        = string
-# }
-
-# variable "node_group_security_groups" {
-#   type        = map(string)
-#   description = "Map of node group names to their security group IDs"
-# }
 
 variable "secret_arn" {
   description = "ARN of the AWS Secrets Manager secret used by the application"
