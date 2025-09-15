e20037b843814b1338044966a8909aca374d3ae8
Adam Levi <adamlevi87@gmail.com>
2025-09-15 20:42:05 +0300

    moved requirements folder

---
+++ terraform-requirements/outputs.tf
@@ -0,0 +1,14 @@
+# .requirements/outputs.tf
+
+# bypassing sensitive - only for testing
+output "aws_iam_openid_connect_provider_github_arn" {
+  description = "ARN of the GitHub OIDC provider (for PROVIDER_GITHUB_ARN secret)"
+  value       = aws_iam_openid_connect_provider.github.arn
+  #sensitive   = true
+}
+
+output "github_oidc_role_arn" {
+  description = "ARN of the GitHub Actions IAM role (for AWS_ROLE_TO_ASSUME secret)"
+  value       = aws_iam_role.github_actions.arn
+  #sensitive   = true
+}
