d5da879e3646052dfde2a67962c215136e725db2
Adam Levi <adamlevi87@gmail.com>
2025-09-16 15:33:58 +0300

    updates for infra

--- terraform-runner-infra/modules/vpc_peering/main.tf
+++ terraform-runner-infra/modules/vpc_peering/main.tf
@@ -48,8 +48,11 @@ resource "aws_vpc_peering_connection" "to_main" {
   count = length(data.terraform_remote_state.main) > 0 ? 1 : 0
 
   vpc_id      = var.source_vpc_id
-  peer_vpc_id = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.vpc_id : null
-  peer_region = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.main_vpc_info.region : null
+  #peer_vpc_id = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.vpc_id : null
+  #peer_vpc_id = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.vpc_id : null
+  peer_vpc_id = try(data.terraform_remote_state.main[0].outputs.main_vpc_info.vpc_id, null)  # Prevents validation error
+  peer_region = try(data.terraform_remote_state.main[0].outputs.main_vpc_info.region, null)
+  #peer_region = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.main_vpc_info.region : null
   #peer_region = var.peer_region
   auto_accept = false  # Will be accepted by the main project
 
@@ -74,6 +77,9 @@ resource "aws_route" "runner_to_main" {
   count = length(data.terraform_remote_state.main) > 0 ? 1 : 0
 
   route_table_id            = var.source_route_table_id
-  destination_cidr_block = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.main_vpc_info.vpc_cidr_block : null
-  vpc_peering_connection_id = length(aws_vpc_peering_connection.to_main) > 0 ? aws_vpc_peering_connection.to_main[0].id : null
+  destination_cidr_block = try(data.terraform_remote_state.main[0].outputs.main_vpc_info.vpc_cidr_block, null)
+  vpc_peering_connection_id = try(aws_vpc_peering_connection.to_main[0].id, null)
+  
+  #destination_cidr_block = length(data.terraform_remote_state.main) > 0 ? data.terraform_remote_state.main[0].outputs.main_vpc_info.vpc_cidr_block : null
+  #vpc_peering_connection_id = length(aws_vpc_peering_connection.to_main) > 0 ? aws_vpc_peering_connection.to_main[0].id : null
 }
--- terraform-runner-infra/modules/vpc_peering/outputs.tf
+++ terraform-runner-infra/modules/vpc_peering/outputs.tf
@@ -3,7 +3,8 @@
 output "vpc_peering_connection_id" {
   description = "ID of the VPC peering connection"
   #value       = aws_vpc_peering_connection.to_main.id
-  value       = length(aws_vpc_peering_connection.to_main) > 0 ? aws_vpc_peering_connection.to_main[0].id : null
+  #value       = length(aws_vpc_peering_connection.to_main) > 0 ? aws_vpc_peering_connection.to_main[0].id : null
+  value       = try(aws_vpc_peering_connection.to_main[0].id, null)
 }
 
 # output "vpc_peering_connection_status" {
